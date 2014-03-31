bd10bf877a6c69e8a2f61ff7bc829bd0692de8b3
omerlive <omerliverant@gmail.com>
2014-03-31 18:58:47 +0300

    first

---
+++ WebSite/JsFunc.js
@@ -0,0 +1,22 @@
+﻿function bigImg(x) {
+    x.style.height = "90px";
+    x.style.width = "90px";
+}
+
+function normalImg(x) {
+    x.style.height = "70px";
+    x.style.width = "70px";
+}
+
+
+function bigImgBorder(x) {
+    x.style.border = '3px solid red';
+    
+}
+
+function normalImgBorder(x) {
+
+    x.style.border = '0px solid red';
+}
+
+
