a18346c5b696d27251da10a5d7dba15446df4d9a
machnes <ido_machnes@walla.co.il>
2014-04-07 13:32:53 +0300

    7.4 מחיקות + השלמות

--- WebSite/Scripts/AutoComplete.js
+++ WebSite/Scripts/AutoComplete.js
@@ -1,72 +1,41 @@
-﻿// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
+﻿//function initialize() {
+
+//    var input = document.getElementById('searchTextField');
+//    var autocomplete = new google.maps.places.Autocomplete(input);
+//}
+
+//google.maps.event.addDomListener(window, 'load', initialize);
+
 
 function initialize() {
 
-    var markers = [];
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+    var input = document.getElementById('cityTb');
+    var autocomplete = new google.maps.places.Autocomplete(input);
 
-    var defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(-33.8902, 151.1759),
-      new google.maps.LatLng(-33.8474, 151.2631));
-    map.fitBounds(defaultBounds);
-
-    // Create the search box and link it to the UI element.
-    var input = /** @type {HTMLInputElement} */(
-      document.getElementById('pac-input'));
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    var searchBox = new google.maps.places.SearchBox(
-    /** @type {HTMLInputElement} */(input));
-
-    // [START region_getplaces]
-    // Listen for the event fired when the user selects an item from the
-    // pick list. Retrieve the matching places for that item.
-    google.maps.event.addListener(searchBox, 'places_changed', function () {
-        var places = searchBox.getPlaces();
-
-        for (var i = 0, marker; marker = markers[i]; i++) {
-            marker.setMap(null);
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        infowindow.close();
+        marker.setVisible(false);
+        input.className = '';
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // Inform the user that the place was not found and return.
+            input.className = 'notfound';
+            return;
         }
 
-        // For each place, get the icon, place name, and location.
-        markers = [];
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0, place; place = places[i]; i++) {
-            var image = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            var marker = new google.maps.Marker({
-                map: map,
-                icon: image,
-                title: place.name,
-                position: place.geometry.location
-            });
-
-            markers.push(marker);
-
-            bounds.extend(place.geometry.location);
+        var address = '';
+        if (place.address_components) {
+            address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
         }
 
-        map.fitBounds(bounds);
-    });
-    // [END region_getplaces]
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
 
-    // Bias the SearchBox results towards places that are within the bounds of the
-    // current map's viewport.
-    google.maps.event.addListener(map, 'bounds_changed', function () {
-        var bounds = map.getBounds();
-        searchBox.setBounds(bounds);
     });
+
+    setupClickListener('changetype-geocode', ['geocode']);
 }
-
 google.maps.event.addDomListener(window, 'load', initialize);
